d8a262abf36c277dbedd13096dfb5689708bd2ae
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-06-02 10:26:56 +0530

    rev Arrays

--- Arrays/reverseArray.cpp
+++ Arrays/reverseArray.cpp
@@ -53,3 +53,41 @@ int main(){
     
 }
 */
+
+// Complexity O(n)
+
+void revSwap(int arr[],int start,int end){
+
+    while(start <= end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = arr[temp];
+        start++;
+        end--;
+
+    }
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int start = 0,end = n-1;
+
+    revSwap(arr,start,end);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] <<endl;
+    }
+    
+    
+
+}
