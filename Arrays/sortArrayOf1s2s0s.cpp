aeffc7d8ec966fd268afa3278ff13c93d57e99fe
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-06-03 11:09:34 +0530

    DSA

---
+++ Arrays/sortArrayOf1s2s0s.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool compa(int a,int b){
+    return a<b;
+
+
+}
+int main() {
+
+    vector<int> arr{0,2,1,2,0};
+    vector<int> arr2;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        /* code */
+        if(arr[i] == 0){
+            arr2.push_back(arr[i]);
+        }
+    }
+    
+
+
+
+    
+    
+    
+
+
+
+}
